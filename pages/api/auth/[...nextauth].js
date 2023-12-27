cf86c20415b0f965198472d0e547efd806582e0f
asadbekbaxshilloyev118 <147907782+asadbekbaxshilloyev118@users.noreply.github.com>
2023-12-27 10:16:37 +0500

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,7 +3,7 @@ import GoogleProvider from 'next-auth/providers/google'
 import {MongoDBAdapter} from "@next-auth/mongodb-adapter";
 import clientPromise from "@/lib/mongodb";
 
-const adminEmails = ['shamshodnurmurodov119@gmail.com'];
+const adminEmails = ['asadbekbaxshilloyev118@gmail.com'];
 
 export const authOptions = {
   secret: process.env.SECRET,
